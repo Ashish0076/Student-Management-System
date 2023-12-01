719e7d7af2f43b9b151cc9e739255bac2cc0e502
Ashish Kumar <ashish9422301@gmail.com>
2023-12-02 01:05:24 +0530

    update

--- Backend/src/main/java/com/BPK/controller/Controller.java
+++ Backend/src/main/java/com/BPK/controller/Controller.java
@@ -1,9 +1,11 @@
 package com.BPK.controller;
 
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,9 +13,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.BPK.model.Student;
 import com.BPK.service.StudentService;
 
+@CrossOrigin(origins = "https://student-management-system-bpk-tech.netlify.app")
 @RestController
 public class Controller {
 
