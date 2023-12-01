284ac5743f7db9137cfd9e8e22f6a738bcdca185
Ashish Kumar <115460698+Ashish0076@users.noreply.github.com>
2023-12-02 00:05:07 +0530

    Update Controller.java

--- Backend/src/main/java/com/BPK/controller/Controller.java
+++ Backend/src/main/java/com/BPK/controller/Controller.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.BPK.model.Student;
 import com.BPK.service.StudentService;
 
+@CrossOrigin(origins = "http://127.0.0.1:5500")
 @RestController
 public class Controller {
 
